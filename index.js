0b5244b3e0192c96d4136ad3da27186acf769a08
Seth Todd <seth.todd@outlook.com>
2021-01-07 21:31:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+let Cat = class {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = function() {
+            return `${this.name} says meow!`
+        }
+    }
+}
+
+let Dog = class {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = function() {
+            return `${this.name} says woof!`
+        }
+    }
+}
+
+let Bird = class {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = function () {
+            if (this.sex === "male") {
+                return `It's me! ${this.name}, the parrot!`
+            } else {
+                return `${this.name} says squawk!`
+            }
+        }
+    }
+}
